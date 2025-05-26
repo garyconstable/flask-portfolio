8756ae836b5371acb07e5aa3ed7f8e317f1b7676
Gary Constable <garyconstable80@gmail.com>
2025-05-26 23:19:18 +0100

    Requirements.

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.secret_key = 'your_secret_key_here'  # Needed for flashing messages
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'garyconstable80@gmail.com'  # Your Gmail address
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
 mail = Mail(app)
